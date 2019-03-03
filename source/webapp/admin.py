bfdef3feb7748b595af3133b5c840c7a45604295
sultanemiluulu <sultanchoponov@gmail.com>
2019-03-03 17:15:52 +0600

    Added models for cinema: hall, movie, show, seat, category; serializers; viewsets; urls

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from webapp.models import Movie
+from webapp.models import Category
 
-# Register your models here.
+
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'name', 'release_date']
+    ordering = ['-release_date']
+    search_fields = ['name', 'id']
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'name']
+    ordering = ['name']
+    search_fields = ['name', 'id']
+
+
+admin.site.register(Movie, MovieAdmin)
+admin.site.register(Category, CategoryAdmin)
+
+
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(max_length=2000, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Movie(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(max_length=2000, null=True, blank=True)
+    poster = models.ImageField(upload_to='posters', null=True, blank=True)
+    release_date = models.DateField()
+    finish_date = models.DateField(null=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='movies', verbose_name="Category", null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Hall(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Seat(models.Model):
+    hall = models.ForeignKey(Hall, on_delete=models.CASCADE, related_name='seats', verbose_name="Hall")
+    row = models.IntegerField()
+    seat = models.IntegerField()
+
+
+class Show(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name='shows', verbose_name="Show")
+    hall = models.ForeignKey(Hall, on_delete=models.CASCADE, related_name='shows', verbose_name="Hall")
+    start_time = models.DateTimeField()
+    finish_time = models.DateTimeField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
